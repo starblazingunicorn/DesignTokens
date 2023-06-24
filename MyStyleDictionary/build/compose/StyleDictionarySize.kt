9b52a9bb2eb7f59bd14c7bc04add74e9a205f800
Viyan.UX Engineer <74237057+starblazingunicorn@users.noreply.github.com>
2023-06-24 11:04:07 +0200

    SD

---
+++ MyStyleDictionary/build/compose/StyleDictionarySize.kt
@@ -0,0 +1,22 @@
+
+
+// Do not edit directly
+// Generated on Sat, 24 Jun 2023 09:00:05 GMT
+
+
+
+package StyleDictionarySize;
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.*
+
+object StyleDictionarySize {
+  /** the base size of the font */
+  val sizeFontBase = 16.00.sp
+  /** the large size of the font */
+  val sizeFontLarge = 32.00.sp
+  /** the medium size of the font */
+  val sizeFontMedium = 16.00.sp
+  /** the small size of the font */
+  val sizeFontSmall = 12.00.sp
+}
---
+++ MyStyleDictionary/build/ios/StyleDictionaryColor.h
@@ -0,0 +1,26 @@
+
+//
+// StyleDictionaryColor.h
+//
+
+// Do not edit directly
+// Generated on Sat, 24 Jun 2023 09:00:05 GMT
+
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
+ColorBaseGrayLight,
+ColorBaseGrayMedium,
+ColorBaseGrayDark,
+ColorBaseRed,
+ColorBaseGreen,
+ColorFontBase,
+ColorFontSecondary,
+ColorFontTertiary
+};
+
+@interface StyleDictionaryColor : NSObject
++ (NSArray *)values;
++ (UIColor *)color:(StyleDictionaryColorName)color;
+@end
